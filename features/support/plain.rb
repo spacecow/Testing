9eb5b5d64e80a7f705c68e052f71b4492a217877
Johan Sveholm <jsveholm@gmail.com>
2009-08-17 23:20:08 +0900

    ready to deploy

--- features/support/plain.rb
+++ features/support/plain.rb
@@ -6,3 +6,11 @@ end
 
 # truncate your tables here if you are using the same database as selenium, since selenium doesn't use transactional fixtures
 Cucumber::Rails.use_transactional_fixtures
+TemplateClass.delete_all
+Course.delete_all
+Klass.delete_all
+Student.delete_all
+Teacher.delete_all
+Person.delete_all
+Attendance.delete_all
+Teaching.delete_all
