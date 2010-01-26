751245cffe7b939bad217d8602aa419dc315657e
Johan Sveholm <jsveholm@gmail.com>
2010-01-27 00:27:34 +0900

    whenever gem

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,3 +46,12 @@ namespace :deploy do
 end
 
 after 'deploy:update_code', 'deploy:symlink_shared'
+
+after "deploy:symlink", "deploy:update_crontab"
+
+namespace :deploy do
+  desc "Update the crontab file"
+  task :update_crontab, :roles => :db do
+    run "cd #{release_path} && whenever --update-crontab #{application}"
+  end
+end
