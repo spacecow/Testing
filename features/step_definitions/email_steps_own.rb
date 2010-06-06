a1292c819fc9535571b5f03a325c3270f62c4fd0
Johan Sveholm <jsveholm@gmail.com>
2010-06-06 14:59:35 +0900

    last month salary should be ok for automatic mails

--- features/step_definitions/email_steps_own.rb
+++ features/step_definitions/email_steps_own.rb
@@ -13,6 +13,7 @@ end
 Then /^the email body should contain "([^"]*?)" in japanese$/ do |text|
   text.gsub!(/#last_month/, ( Time.zone.now-1.month ).month.to_s)
   text.gsub!(/#this_month/, Time.zone.now.month.to_s)
-  text.gsub!(/#confirm_day/, ( Time.zone.now.beginning_of_month+5.day ).strftime( "%a" ))
+  #text.gsub!(/#confirm_day/, ( Time.zone.now.beginning_of_month+5.day ).strftime( "%a" ))
+  text.gsub!(/#confirm_day/, %w(日 月 火 水 木 金 土)[(Time.zone.now.beginning_of_month+5.day).wday])
   Then "I should see \"#{text}\" in the email body"
 end
